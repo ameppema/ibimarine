3d1c942264df6a0b79bafa76c05025c57c67a40e
Jean Langarica <jeanmacario048@gmail.com>
2022-03-31 22:25:56 -0600

    Added Contact, Events & Toys sections

---
+++ resources/views/page/events.blade.php
@@ -0,0 +1,112 @@
+@extends('layout.template')
+
+@section('title', 'Eventos')
+
+@section('hero')
+    <!-- Hero -->
+    <div class="hero__content">
+        <div class="hero-container hero-container-text">
+            <h1 class="margin-1 text-title text-center text-upper text-bold text-color-black">Eventos</h1>
+        </div>
+    </div>
+@endsection
+
+@section('content')
+    <!-- Section Content -->
+    <div class="content">
+        <div class="container home__grid">
+            <!-- Card -->
+            <div class="card">
+                <div class="card-head">
+                    <div class="card__img">
+                        <img class="img-fluid smooth-top-corners" src="img/events/events-07.jpg" alt="card image">
+                    </div>
+                </div>
+                <div class="card-body-no-border">
+                    <div class="card__text text-center margin-bottom-1">
+                        <p class="text-color-black margin-vertical-1 padding-horizontal-1">
+                            Cras venenatis porta ligula sed suscipit.
+                            Ut in vehicula ex. Vivamus fermentum nunc mi,
+                            ac condimentum magna porttitor faucibus.
+                            <br>
+                            <br>
+                            Vestibulum ante ipsum primis in faucibus
+                            orci luctus et ultrices posuere cubila curae;
+                            sed non lorem at felis tincidunt lacinia nec
+                            id leo fermentum quam gravida ut.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <!-- Card -->
+            <div class="card">
+                <div class="card-head">
+                    <div class="card__img">
+                        <img class="img-fluid smooth-top-corners" src="img/events/events-01.jpg" alt="card image">
+                    </div>
+                </div>
+                <div class="card-body-no-border">
+                    <div class="card__text text-center margin-bottom-1">
+                        <p class="text-color-black margin-vertical-1 padding-horizontal-1">
+                            Cras venenatis porta ligula sed suscipit.
+                            Ut in vehicula ex. Vivamus fermentum nunc mi,
+                            ac condimentum magna porttitor faucibus.
+                            <br>
+                            <br>
+                            Vestibulum ante ipsum primis in faucibus
+                            orci luctus et ultrices posuere cubila curae;
+                            sed non lorem at felis tincidunt lacinia nec
+                            id leo fermentum quam gravida ut.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <!-- Card -->
+            <div class="card">
+                <div class="card-head">
+                    <div class="card__img">
+                        <img class="img-fluid smooth-top-corners" src="img/events/events-14.jpg" alt="card image">
+                    </div>
+                </div>
+                <div class="card-body-no-border">
+                    <div class="card__text text-center margin-bottom-1">
+                        <p class="text-color-black margin-vertical-1 padding-horizontal-1">
+                            Cras venenatis porta ligula sed suscipit.
+                            Ut in vehicula ex. Vivamus fermentum nunc mi,
+                            ac condimentum magna porttitor faucibus.
+                            <br>
+                            <br>
+                            Vestibulum ante ipsum primis in faucibus
+                            orci luctus et ultrices posuere cubila curae;
+                            sed non lorem at felis tincidunt lacinia nec
+                            id leo fermentum quam gravida ut.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <!-- Card -->
+            <div class="card">
+                <div class="card-head">
+                    <div class="card__img">
+                        <img class="img-fluid smooth-top-corners" src="img/events/events-04.jpg" alt="card image">
+                    </div>
+                </div>
+                <div class="card-body-no-border">
+                    <div class="card__text text-center margin-bottom-1">
+                        <p class="text-color-black margin-vertical-1 padding-horizontal-1">
+                            Cras venenatis porta ligula sed suscipit.
+                            Ut in vehicula ex. Vivamus fermentum nunc mi,
+                            ac condimentum magna porttitor faucibus.
+                            <br>
+                            <br>
+                            Vestibulum ante ipsum primis in faucibus
+                            orci luctus et ultrices posuere cubila curae;
+                            sed non lorem at felis tincidunt lacinia nec
+                            id leo fermentum quam gravida ut.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/page/toys.blade.php
@@ -0,0 +1,150 @@
+@extends('layout.template')
+
+@section('title', 'Toys')
+
+@section('hero')
+    <!-- Hero -->
+    <div class="hero__content">
+        <div class="hero-container hero-container-text">
+            <h1 class="margin-1 text-title text-center text-upper text-bold text-color-black">Complementos Náuticos</h1>
+        </div>
+    </div>
+@endsection
+
+@section('content')
+    <!-- Section Content -->
+    <div class="content">
+        <div class="container home__grid">
+            <!-- Card -->
+            <div class="card">
+                <div class="card-head">
+                    <div class="card__img">
+                        <img class="img-fluid smooth-top-corners" src="img/complements/complements-08.jpeg" alt="card image">
+                    </div>
+                    <div class="card__title">
+                        <p class="text-center text-upper">Moto Acuatica</p>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="card__text text-center margin-bottom-1">
+                        <p class="text-color-black margin-vertical-1 padding-horizontal-1">
+                            Cras venenatis porta ligula sed suscipit.
+                            Ut in vehicula ex. Vivamus fermentum nunc mi,
+                            ac condimentum magna porttitor faucibus.
+                            <br>
+                            <br>
+                            Vestibulum ante ipsum primis in faucibus
+                            orci luctus et ultrices posuere cubila curae;
+                            sed non lorem at felis tincidunt lacinia nec
+                            id leo fermentum quam gravida ut.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <!-- Card -->
+            <div class="card">
+                <div class="card-head">
+                    <div class="card__img">
+                        <img class="img-fluid smooth-top-corners" src="img/complements/complements-07.jpeg" alt="card image">
+                    </div>
+                    <div class="card__title">
+                        <p class="text-center text-upper">Paddel Surf</p>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="card__text text-center margin-bottom-1">
+                        <p class="text-color-black margin-vertical-1 padding-horizontal-1">
+                            Cras venenatis porta ligula sed suscipit.
+                            Ut in vehicula ex. Vivamus fermentum nunc mi,
+                            ac condimentum magna porttitor faucibus.
+                            <br>
+                            <br>
+                            Vestibulum ante ipsum primis in faucibus
+                            orci luctus et ultrices posuere cubila curae;
+                            sed non lorem at felis tincidunt lacinia nec
+                            id leo fermentum quam gravida ut.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <!-- Card -->
+            <div class="card">
+                <div class="card-head">
+                    <div class="card__img">
+                        <img class="img-fluid smooth-top-corners" src="img/complements/complements-10.jpg" alt="card image">
+                    </div>
+                    <div class="card__title">
+                        <p class="text-center text-upper">Sea Bot</p>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="card__text text-center margin-bottom-1">
+                        <p class="text-color-black margin-vertical-1 padding-horizontal-1">
+                            Cras venenatis porta ligula sed suscipit.
+                            Ut in vehicula ex. Vivamus fermentum nunc mi,
+                            ac condimentum magna porttitor faucibus.
+                            <br>
+                            <br>
+                            Vestibulum ante ipsum primis in faucibus
+                            orci luctus et ultrices posuere cubila curae;
+                            sed non lorem at felis tincidunt lacinia nec
+                            id leo fermentum quam gravida ut.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <!-- Card -->
+            <div class="card">
+                <div class="card-head">
+                    <div class="card__img">
+                        <img class="img-fluid smooth-top-corners" src="img/complements/complements-02.jpg" alt="card image">
+                    </div>
+                    <div class="card__title">
+                        <p class="text-center text-upper">Banana Náutica</p>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="card__text text-center margin-bottom-1">
+                        <p class="text-color-black margin-vertical-1 padding-horizontal-1">
+                            Cras venenatis porta ligula sed suscipit.
+                            Ut in vehicula ex. Vivamus fermentum nunc mi,
+                            ac condimentum magna porttitor faucibus.
+                            <br>
+                            <br>
+                            Vestibulum ante ipsum primis in faucibus
+                            orci luctus et ultrices posuere cubila curae;
+                            sed non lorem at felis tincidunt lacinia nec
+                            id leo fermentum quam gravida ut.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <!-- Card -->
+            <div class="card">
+                <div class="card-head">
+                    <div class="card__img">
+                        <img class="img-fluid smooth-top-corners" src="img/complements/complements-11.jpeg" alt="card image">
+                    </div>
+                    <div class="card__title">
+                        <p class="text-center text-upper">Inflables</p>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="card__text text-center margin-bottom-1">
+                        <p class="text-color-black margin-vertical-1 padding-horizontal-1">
+                            Cras venenatis porta ligula sed suscipit.
+                            Ut in vehicula ex. Vivamus fermentum nunc mi,
+                            ac condimentum magna porttitor faucibus.
+                            <br>
+                            <br>
+                            Vestibulum ante ipsum primis in faucibus
+                            orci luctus et ultrices posuere cubila curae;
+                            sed non lorem at felis tincidunt lacinia nec
+                            id leo fermentum quam gravida ut.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
