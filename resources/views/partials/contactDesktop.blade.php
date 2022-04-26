b9e731e1d5f49f85c8bffd0d81b940af6ff30243
Jean Langarica <jeanmacario048@gmail.com>
2022-04-26 02:17:08 -0500

    Minor line separator fixed in lang and only desktop contact section

--- resources/views/partials/contactDesktop.blade.php
+++ resources/views/partials/contactDesktop.blade.php
@@ -11,7 +11,7 @@
                     <img class="w-8 mr-1 h-[22px] " src="/img/lang_es.png" alt="">
                     <p class="font-bold text-xl text-old-black">+34 650 020 215</p>
                 </div>
-                <div class="text-old-black mx-3 font-bold">&#124;</div>
+                <div class="text-old-black mx-3 font-bold" style="font-size: x-large;">&#124;</div>
                 <div class="flex items-center">
                     <img class="w-8 mr-1 h-[22px] " src="/img/lang_en.png" alt="">
                     <p class="font-bold text-xl text-old-black">+34 650 020 215</p>
