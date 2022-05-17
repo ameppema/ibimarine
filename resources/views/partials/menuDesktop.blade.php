7f57d67603263eef019de4eb1684cd2ce7ccd9e1
Jean Langarica <jeanmacario048@gmail.com>
2022-05-17 16:41:22 -0500

    Added Hover on Menu items on Desktop

--- resources/views/partials/menuDesktop.blade.php
+++ resources/views/partials/menuDesktop.blade.php
@@ -2,7 +2,7 @@
     <div class="lg:w-11/12 xl:w-4/5 mx-auto mt-8 border-t-4 border-old-gold pt-4">
         <ul class="flex justify-around items-center text-old-black ">
             @foreach (TheMenu() as $item)
-                <li class="{{ (request()->routeIs($item->route) || request()->routeIs($item->route . '.*')) ? 'active-link' : ''}} lg:py-0 xl:py-1 lg:px-3"><a class="uppercase text-sm " href="{{ route($item->route)}}"> {{$item->name}} </a></li>
+                <li class="{{ (request()->routeIs($item->route) || request()->routeIs($item->route . '.*')) ? 'active-link' : ''}} lg:py-0 xl:py-1 lg:px-3 hover:active-link"><a class="uppercase text-sm " href="{{ route($item->route)}}"> {{$item->name}} </a></li>
             @endforeach
             @if (auth()->user())  
             <form method="POST" action="{{route('logout')}}">
