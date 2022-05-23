e01fe837dcd17525e93efa6eb1baf81215cd4ab6
Jean Langarica <jeanmacario048@gmail.com>
2022-05-23 15:45:40 -0500

    Hotfix\ Fixed Main Brand Image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -131,7 +131,7 @@
     <div class="xl:last:ml-0 w-4/5 lg:w-full mx-auto my-6 md:max-w-[340px] xl:max-w-[30rem]  sm:max-w-[300px] lg:max-w-md">
         <div class="relative lg:h-56 xl:h-72">
             <div class="rounded-t-lg max-h-[200px] xl:max-h-64 overflow-y-hidden">
-                <img class="rounded-t-[12px]"  src="./img/contact/contact_ibimarine.jpg" alt="">
+                <img class="rounded-t-[12px]"  src="./img/home/contact.jpg" alt="">
             </div>
             <div class="absolute bottom-0 h-8 xl:h-12 bg-old-gold w-full text-center">
                 <p class="text-white leading-8 xl:leading-[3rem] text-lg xl:text-2xl uppercase">{{__('Contact')}}</p>
